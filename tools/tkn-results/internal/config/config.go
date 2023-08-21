8bb89580ee703ebfbb409444edd1821c2153cc19
Khurram Baig <kbaig@redhat.com>
2023-08-21 17:45:28 +0530

    [CLI] Fix the broken flags
    Earlier, we weren't calling getconfig in the beginning. After we added
    port-forward, we added GetConfig call earlier. This fixed the global flag
    and when we merge global flag fix, it broke the local flag. Now, refactored
    the setting flag to singleton in the init to avoid undefined scenarios.

--- tools/tkn-results/internal/config/config.go
+++ tools/tkn-results/internal/config/config.go
@@ -21,6 +21,7 @@ var (
 		EnvSSLRootFilePath:       "Path to local SSL cert to use.",
 		EnvSSLServerNameOverride: "SSL server name override (useful if using with a proxy such as kubectl port-forward).",
 	}
+	cfg *Config
 )
 
 type Config struct {
@@ -55,19 +56,20 @@ func init() {
 	viper.SetConfigName("results")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("$HOME/.config/tkn")
-	pflag.StringP("addr", "a", "", "Result API server address")
-	pflag.StringP("authtoken", "t", "", "authorization bearer token to use for authenticated requests")
-	pflag.Parse()
+	err := setConfig()
+	if err != nil {
+		fmt.Println("error setting up flags and config", err)
+	}
 }
 
-func GetConfig() (*Config, error) {
+func setConfig() error {
 	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
-		return nil, err
+		fmt.Println(err)
 	}
 
 	for k := range env {
 		if err := viper.BindEnv(k); err != nil {
-			return nil, err
+			return err
 		}
 	}
 
@@ -78,7 +80,7 @@ func GetConfig() (*Config, error) {
 
 	// Initial config is contains the env variables,
 	// so that the unmarshal can take priority if those values are set.
-	cfg := &Config{
+	cfg = &Config{
 		SSL: SSLConfig{
 			RootsFilePath:      viper.GetString(EnvSSLRootFilePath),
 			ServerNameOverride: viper.GetString(EnvSSLServerNameOverride),
@@ -87,7 +89,8 @@ func GetConfig() (*Config, error) {
 
 	if err := viper.ReadInConfig(); err == nil {
 		if err := viper.Unmarshal(cfg); err != nil {
-			return nil, err
+			fmt.Println(err)
+			return err
 		}
 	} else {
 		fmt.Println(err)
@@ -101,6 +104,9 @@ func GetConfig() (*Config, error) {
 		cfg.Token = viper.GetString("authtoken")
 	}
 	cfg.Portforward = viper.GetBool("portforward")
+	return nil
+}
 
+func GetConfig() (*Config, error) {
 	return cfg, nil
 }
